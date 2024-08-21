8473517aba4c6966982b77d8bb02c3332fead7c4
João Rhoden <j.machado02@edu.pucrs.br>
2024-08-21 01:04:28 -0300

    att

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser'
+    executablePath: '/usr/bin/chromium-browser',
+    headless:false,
+    args: ["--no-sandbox"]
   })
   const page = await browser.newPage();
   
