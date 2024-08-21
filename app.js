3e86fa2bafdd10882f7f9211cd57cdd3d326884e
João Rhoden <j.machado02@edu.pucrs.br>
2024-08-21 01:57:29 -0300

    att

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
   
   await page.goto('https://www.tipminer.com/historico/pragmatic/roleta-brasileira?limit=1&t=1724209457366&subject=filter');
