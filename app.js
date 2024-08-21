87d4261747db05ad3b2ecf8d12de8195feffa2a5
João Rhoden <j.machado02@edu.pucrs.br>
2024-08-21 01:10:42 -0300

    att

--- app.js
+++ app.js
@@ -2,16 +2,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: 'new',
-    args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--disable-gpu',
-    ],
+    headless: true, 
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-audio-output']
   })
   const page = await browser.newPage();
   
